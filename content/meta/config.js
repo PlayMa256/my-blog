c84e8932b2d7a0ad18fd650d10ba2bd631b61f2a
PlayMa256 <matheus.g.silva@hotmail.com>
2019-11-27 11:21:59 -0200

    update config

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "Matheus Gonçalves da Silva",
+  shortSiteTitle: "Matheus Gonçalves da Silva", // <title> ending for posts and pages
+  siteDescription: "",
+  siteUrl: "https://playMa256.github.io/myblog",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Matheus Gonçalves da Silva",
+  authorTwitterAccount: "matheus1lva",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Matheus Gonçalves da Silva",
+  headerSubTitle: "My personal blog, with insights and last trends",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Matheus Gonçalves da Silva - blog",
+  manifestShortName: "MyBlog", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -32,8 +32,7 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/PlayMa256" },
+    { name: "twitter", url: "https://twitter.com/matheus1lva" }
   ]
 };
