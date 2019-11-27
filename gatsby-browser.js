d5754d0c5275b4a91d6571664458ecf91a42dd25
PlayMa256 <matheus.g.silva@hotmail.com>
2019-11-27 12:37:13 -0200

    add remark

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+require("prismjs/themes/prism-solarizedlight.css");
+require("prismjs/plugins/line-numbers/prism-line-numbers.css");
